ce8abe91bc477624b59b67f4cebbaa9c6beac636
Mark Lhester Agellon <kianirel56@gmail.com>
2023-01-14 17:35:35 +0800

    Added method -> add_child()

--- Binary Tree – Class.py
+++ Binary Tree – Class.py
@@ -1,7 +1,25 @@
 class BinarySearchTreenode():
+    # This part is used to create instance object
+    # It basicall automatically creates properties that all object will take
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
 
-    
+    # This is a method for adding or reading a data 
+    def add_child(self, data):
+        if data == self.data:
+            return
+        
+        if data < self.data:
+            # Add data to the left subtree
+            if self.left:
+                self.left.add_child( data)
+            else:
+                self.left = BinarySearchTreenode(data)
+        else:
+            # Add data to the right subtree
+            if self.right:
+                self.right.add_child( data)
+            else:
+                self.right = BinarySearchTreenode(data)           
